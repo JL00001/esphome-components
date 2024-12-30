7ad5e60d813a701c314bd860f8601b9740e27d02
JL00001 <44040750+JL00001@users.noreply.github.com>
2024-12-30 15:59:14 -0500

    Update sensor.py

--- components/max17048/sensor.py
+++ components/max17048/sensor.py
@@ -47,6 +47,12 @@ CONFIG_SCHEMA = (
                 device_class=DEVICE_CLASS_BATTERY,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
+            cv.Optional(CONF_CONFIGURATION): sensor.sensor_schema(
+                unit_of_measurement=f"0x",
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_BATTERY,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
             cv.Optional(CONF_VERSION): sensor.sensor_schema(
                 unit_of_measurement=UNIT_PERCENT,
                 accuracy_decimals=1,
